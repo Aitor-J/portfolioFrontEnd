686274da31ad06b8eff8131e8b99c90814af6259
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-06-30 20:03:12 +0200

    adding about me section started, resolving navbar bug on z-index and translateX

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,19 @@ const About = () => {
   return (
     <div className="about">
       <div className="about__container">
-        <img className="about__container__img" src="/assets/profilPic.png" alt="me,myself and I" />
-        <div className="about__container__text" >
+        <img
+          className="about__container__img"
+          src="/assets/profilPic.png"
+          alt="me,myself and I"
+        />
+        <div className="about__container__text">
           <h1 className="about__container__text__h1">About me</h1>
-          <p className="about__container__text__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente omnis voluptates, ducimus expedita consectetur assumenda sed laudantium cum sunt repellendus dicta explicabo dolores ea incidunt, vel accusamus? Repudiandae, sed blanditiis.</p>
+          <p className="about__container__text__p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            omnis voluptates, ducimus expedita consectetur assumenda sed
+            laudantium cum sunt repellendus dicta explicabo dolores ea incidunt,
+            vel accusamus? Repudiandae, sed blanditiis.
+          </p>
           <ul className="about__container__text__ul">
             <li className="about__container__text__ul__li">Web technologies</li>
             <li className="about__container__text__ul__li">Soft skills</li>
