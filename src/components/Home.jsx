a7aeebfb925344ef6fb3e77894fb6e57e838ac44
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-06-30 19:04:52 +0200

    resolving burger bug and starting home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home__container">
-      <h3 className="home__container__h3">Welcome to my world</h3>
-      <h1 className="home__container__h1">Hi, i'm Aitor Jurquet</h1>
-      <span className="home__container__span">Web Developper</span>
-      <h1 className="home__container__h1">Based in bask country</h1>
+        <h3 className="home__container__h3">Welcome to my world</h3>
+        <h1 className="home__container__h1">Hi, im Aitor Jurquet</h1>
+        <span className="home__container__span">Web Developper</span>
+        <h1 className="home__container__h1">Based in bask country</h1>
       </div>
     </div>
   );
