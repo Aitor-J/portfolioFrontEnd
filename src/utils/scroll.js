2494617510f929c1282a21718bb784184560a12b
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-06-30 13:35:36 +0200

    adding ref to all sections

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,9 +1,9 @@
 const handleScroll = (ref) => {
-    return window.scrollTo({
-      top: ref?.current.offsetTop,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
-  
-  export default handleScroll;
+  return window.scrollTo({
+    top: ref?.current.offsetTop,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+export default handleScroll;
